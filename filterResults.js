fa7abac5396827cc42041fc2dd66420c0a76e69c
Jay Kelly <comital333@gmail.com>
2019-03-08 14:24:23 +1000

    added template for getting data from the different shops

--- filterResults.js
+++ filterResults.js
@@ -5,14 +5,20 @@ function filterResults(items) {
 		const a = item.querySelector('div.na4ICd div.eWxN4b a')
 		return a ? {text: a.innerText, link: a.href} : null
 		
-    }
+	}
+	
+	// get links to all shops
+	function getShops(item){
+		
+	}
     // title: product name, link: price comparison page or product page (if only one result)
     // price: lowest price, description: product description, reviews: link to reviews and amount
 	return items.map( item => 
 		({
 				title: item.querySelector("div.eIuuYe a").innerText,
 				link: item.querySelector("div.eIuuYe a").getAttribute('href'),
-                price: item.querySelector("div.mQ35Be").innerText,
+				price: item.querySelector("div.mQ35Be span").innerText,
+				shops: item.querySelector("div.mQ35Be").innerText,
 				description: item.querySelector('div.na4ICd:not(.TxCHDf)').innerText,
 				thumbnail: item.querySelector('div.sh-dlr__thumbnail img').src,
 				reviews: getReviews(item)

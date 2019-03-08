c22b89502057ccd919125943bf9012ef58768250
Jay Kelly <comital333@gmail.com>
2019-03-08 15:17:14 +1000

    use page.click() instead of page.goto()

--- index.js
+++ index.js
@@ -4,36 +4,43 @@ const getNextUrl = require('./pages').getNextUrl
 
 const scrape = async options => {
 
-	async function recursiveScrape(url, callback){
+	async function recursiveScrape(page, callback){
         console.log('scrape')
-        const page = await browser.newPage()
-        await page.goto(url)
-        const nextUrl = await getNextUrl(page)
-        console.log('nextUrl' + nextUrl)
-        const results = nextUrl ? await recursiveScrape(nextUrl, callback) : []
+        // return empty if no more pages
+        try {
+          await page.waitForSelector('tbody > tr > .navend > #pnnext > span:nth-child(2)')
+        } catch(e) {
+          return []
+        }
+        // scrape the page
         const result = await page.$$eval('div.sh-dlr__list-result', callback)
+        // navigate to the next page
+        await page.click('tbody > tr > .navend > #pnnext > span:nth-child(2)')
+        // recursive call fro results
+        const results = await recursiveScrape(page, callback)
         results.push(result)
         return results
     }
-  const browser = await puppeteer.launch({ headless: true });
-  // const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
   const url = `https://www.google.com.au/search?q=${options.query}&&tbm=shop`;
 
-  // await page.goto(url);
+  await page.goto(url);
 
 	// // gets list of results as Array and passes it to filterResults()
 	// const results = await page.$$eval('div.sh-dlr__list-result', filterResults)
 
-  recursiveScrape(url, filterResults).then(results => {
+  recursiveScrape(page, filterResults).then(results => {
     const set = new Set
-    console.log(results)
+    // console.log(results)
     results.map(result => {
       result.map(product => {
-        console.log(product)
+        set.add(product)
       })
     })
     console.log(set)
+    console.log(set.size)
   })
   
 	
